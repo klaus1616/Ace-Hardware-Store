9cdeeb242c06fab2d8269d6be289df76a638732d
klaudiusz lech <klaudlech@89gmail.com>
2024-05-23 00:23:28 -0500

    Fix

---
+++ docs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"com.example.AceHardwareStore"},{"l":"com.example.AceHardwareStore.controllers"},{"l":"com.example.AceHardwareStore.daos"},{"l":"com.example.AceHardwareStore.exceptions"},{"l":"com.example.AceHardwareStore.models"}];updateSearchResults();
